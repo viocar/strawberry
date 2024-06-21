a5f2a033bb80ed26bf10d92a148400dab45ec335
Jonas Kvinge <jonas@jkvinge.net>
2024-06-21 16:36:40 +0200

    Add visualizations

--- src/core/mainwindow.h
+++ src/core/mainwindow.h
@@ -101,6 +101,7 @@ class Windows7ThumbBar;
 class AddStreamDialog;
 class LastFMImportDialog;
 class RadioViewContainer;
+class VisualizationContainer;
 
 class MainWindow : public QMainWindow, public PlatformInterface {
   Q_OBJECT
@@ -270,6 +271,7 @@ class MainWindow : public QMainWindow, public PlatformInterface {
  public Q_SLOTS:
   void CommandlineOptionsReceived(const QByteArray &string_options);
   void Raise();
+  void ShowVisualizations();
 
  private:
 
@@ -350,6 +352,10 @@ class MainWindow : public QMainWindow, public PlatformInterface {
   RadioViewContainer *radio_view_;
 
   LastFMImportDialog *lastfm_import_dialog_;
+
+#ifdef HAVE_VISUALIZATIONS
+  ScopedPtr<VisualizationContainer> visualization_;
+#endif
 
   QAction *collection_show_all_;
   QAction *collection_show_duplicates_;
